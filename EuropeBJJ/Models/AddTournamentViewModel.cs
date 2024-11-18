acb8c92316e4dee980d8e41ed5e84e69445e1396
YovoTWW <yovonedelchev@gmail.com>
2024-11-18 16:27:47 +0200

    filled AddTournamentViewModel

--- EuropeBJJ/Models/AddTournamentViewModel.cs
+++ EuropeBJJ/Models/AddTournamentViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace EuropeBJJ.Models
+﻿using System.ComponentModel.DataAnnotations;
+using static EuropeBJJ.Constants.ModelConstants;
+
+namespace EuropeBJJ.Models
 {
     public class AddTournamentViewModel
     {
+        [MaxLength(MaxEventNameLength)]
+        public string Name { get; set; } = null!;
 
+        [MaxLength(MaxNameLength)]
+        public string Country { get; set; } = null!;
+
+        [MaxLength(MaxNameLength)]
+        public string City { get; set; } = null!;
+
+        public string? Image { get; set; }
+
+        public string Link { get; set; } = null!;
+
+        public string Date { get; set; } = null!;
     }
 }
